994f9e86fb2ef66cbeb73961ea3a4c6f627cbd23
iAverages <me@danielraybone.com>
2024-12-22 20:58:42 +0000

    add direct page for image, add testing page

---
+++ pages/dan.tsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+
+// <meta name="og:title" content={track.name} />
+// <meta name="og:description" content={track.artists[0].name} //>
+// <meta name="og:image" content={og} //>
+//
+// <meta name="twitter:card" content="summary_large_image" //>
+// <meta name="twitter:title" content={track.name} //>
+// <meta name="twitter:description" content={track.artists[0].name} //>
+// <meta name="twitter:image" content={og} //>
+
+const Dan = () => {
+  return (
+    <>
+      <Head>
+        <meta property="og:site_name" content="og:site_name" />
+        <meta property="og:title" content="og:title" />
+        <meta property="og:description" content="og:description" />
+        <meta property="description" content="description" />
+        <meta property="og:url" content="https://danielraybone.com" />
+        <meta property="theme-color" content="#7e22ce" />
+        <meta property="og:image" content="https://avrg.dev/a3nUaH.png" />
+        <meta property="og:type" content="video" />
+        <meta property="og:video" content="https://avrg.dev/X0uLDq.mp4" />
+        <meta property="og:video:type" content="video/mp4" />
+        <meta property="og:video:height" content="250" />
+        <meta property="og:video:width" content="306" />
+        <meta
+          property="og:video:secure_url"
+          content="https://avrg.dev/X0uLDq.mp4"
+        />
+      </Head>
+      <div>testing</div>
+    </>
+  );
+};
+
+export default Dan;
