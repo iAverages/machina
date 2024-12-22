cce710d9545baf5dbe73c8a4312e50d55c310c3b
iAverages <me@danielraybone.com>
2024-12-22 03:40:15 +0000

    get spotify data

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,11 +1,97 @@
 import { GetServerSidePropsContext } from "next";
 import Head from "next/head";
 
+const getSpotifyAuth = async () => {
+  const client_id = process.env.client_id;
+  const client_secret = process.env.client_secret;
+
+  const authOptions = {
+    method: "POST",
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(`${client_id}:${client_secret}`).toString("base64"),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      grant_type: "client_credentials",
+    }),
+  };
+
+  const response = await fetch(
+    "https://accounts.spotify.com/api/token",
+    authOptions,
+  );
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  const data = await response.json();
+
+  const token = data.access_token;
+  console.log("Access token:", token);
+};
+
+const getTrackIdFromUrl = (url: string) => {
+  const regex = /https:\/\/open\.spotify\.com\/track\/([a-zA-Z0-9]+)(\?.*)?/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
+};
+
+export interface TrackApi {
+  album: Album;
+  artists: Artist[];
+  id: string;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  type: string;
+  uri: string;
+}
+
+export interface Album {
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: Image[];
+}
+
+export interface ExternalUrls {
+  spotify: string;
+}
+
+export interface Image {
+  url: string;
+  height: number;
+  width: number;
+}
+
+export interface Artist {
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+}
+
 export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const token = await getSpotifyAuth();
+  const trackId = getTrackIdFromUrl(context.params.slug[0]);
+  const url = `https://api.spotify.com/v1/tracks/${trackId}`;
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = (await response.json()) as TrackApi;
   const base = "https://s.kirsi.dev";
-  const og =
-    base +
-    "/api/og?albumArt=https%3A%2F%2Fimage-cdn-ak.spotifycdn.com%2Fimage%2Fab67616d00001e0218285df0b7581a0fcc806afc&artist=%E5%A0%95%E5%A4%A9&songName=Creepy%20Nuts";
+  const params = new URLSearchParams();
+  params.set("albumArt", data.album.images[0].url);
+  params.set("artist", data.artists[0].name);
+  params.set("songName", data.name);
+  const og = `${base}/api/og?${params.toString()}`;
 
   return {
     props: {
