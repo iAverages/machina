c194f6afad1b9f07dda716d4166ae6e4c0a5bd63
iAverages <me@danielraybone.com>
2025-03-09 16:24:07 +0000

    restructure flake slightly

--- flake.nix
+++ flake.nix
@@ -2,59 +2,47 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "https://flakehub.com/f/oxalica/rust-overlay/*.tar.gz";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     nixpkgs,
     rust-overlay,
+    flake-utils,
     ...
-  }: let
-    allSystems = [
-      "x86_64-linux"
-    ];
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [rust-overlay.overlays.default];
+      };
 
-    forEachSystem = f:
-      nixpkgs.lib.genAttrs allSystems (system:
-        f {
-          inherit system;
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlays.default
-            ];
-          };
-        });
-  in {
-    devShells = forEachSystem ({
-      pkgs,
-      system,
-    }: {
-      default = with pkgs;
+      pnpm = pkgs.nodePackages.pnpm;
+
+      rust = pkgs.rust-bin.stable.latest.default.override {
+        extensions = ["rust-src" "rust-analyzer"];
+        targets = ["x86_64-unknown-linux-gnu"];
+      };
+    in {
+      devShells.default = with pkgs;
         mkShell {
-          shellHook = ''
-            export  PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
-
-            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine";
-            export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine";
-            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
-            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-          '';
-
-          packages = with pkgs; [
-            nodejs_22
+          packages = [
+            nodejs
             pnpm
             pkg-config
             openssl
             just
             mprocs
-            (rust-bin.stable.latest.default.override {
-              extensions = [
-                "rust-src"
-                "rust-analyzer"
-              ];
-            })
+            rust
           ];
+
+          shellHook = ''
+            export  PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
+            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine";
+            export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine";
+            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+          '';
         };
     });
-  };
 }
