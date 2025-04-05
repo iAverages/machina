53b46ba5e4421bc39def3bea51623c375b82951f
iAverages <me@danielraybone.com>
2025-04-05 21:38:33 +0100

    add node_modules/.bin to PATH

--- flake.nix
+++ flake.nix
@@ -87,6 +87,7 @@
             export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine";
             export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
             export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+            export PATH="$PWD/node_modules/.bin/:$PATH"
           '';
         };
     });
