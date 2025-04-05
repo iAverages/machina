798cd1f900e1ff346535122b84364a86a28a9c4b
iAverages <me@danielraybone.com>
2025-04-05 23:53:48 +0100

    fix type errors from front due to og package
    typescript is a pain in the ass holy shit

--- packages/og/vite.config.ts
+++ packages/og/vite.config.ts
@@ -1,9 +1,21 @@
 import { resolve } from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import dtsPlugin from "vite-plugin-dts";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        dtsPlugin({
+            compilerOptions: {
+                tsBuildInfoFile: "tsconfig.tsbuildinfo",
+                outDir: "dist",
+                rootDir: "src",
+                noEmit: false,
+            },
+            include: ["src/**/*.ts", "src/**/*.tsx"],
+        }),
+    ],
     build: {
         target: "esnext",
         minify: false,
